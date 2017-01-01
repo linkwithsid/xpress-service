c367f7482c30ecc10775fe199cf6f47fade1096a
shubham <super.shubham.7@gmail.com>
2017-01-01 22:03:23 +0530

    some basic refactor

--- src/main/java/com/exam/xpress/web/HomeController.java
+++ src/main/java/com/exam/xpress/web/HomeController.java
@@ -2,7 +2,6 @@ package com.exam.xpress.web;
 
 import java.util.Map;
 
-import com.exam.xpress.service.HelloWorldService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,37 +15,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class WelcomeController {
 
 	private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
-	private final HelloWorldService helloWorldService;
-
-	@Autowired
-	public WelcomeController(HelloWorldService helloWorldService) {
-		this.helloWorldService = helloWorldService;
-	}
-
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Map<String, Object> model) {
-
 		logger.debug("index() is executed!");
-
-		model.put("title", helloWorldService.getTitle(""));
-		model.put("msg", helloWorldService.getDesc());
-		
 		return "index";
-	}
-
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
-	public ModelAndView hello(@PathVariable("name") String name) {
-
-		logger.debug("hello() is executed - $name {}", name);
-
-		ModelAndView model = new ModelAndView();
-		model.setViewName("index");
-		
-		model.addObject("title", helloWorldService.getTitle(name));
-		model.addObject("msg", helloWorldService.getDesc());
-		
-		return model;
-
 	}
 
 }
