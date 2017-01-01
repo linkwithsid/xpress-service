79205c2547e84f07b979b2b547fb70f518435e98
shubham <super.shubham.7@gmail.com>
2017-01-01 22:06:09 +0530

    some basic refactor

--- src/main/java/com/exam/xpress/web/HomeController.java
+++ src/main/java/com/exam/xpress/web/HomeController.java
@@ -4,17 +4,14 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class WelcomeController {
+public class HomeController {
 
-	private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Map<String, Object> model) {
 		logger.debug("index() is executed!");
