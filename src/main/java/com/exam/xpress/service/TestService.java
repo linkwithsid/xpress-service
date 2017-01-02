9f7d9751a35ccc76b8e638e711a076d179f3dc4f
shubham <super.shubham.7@gmail.com>
2017-01-02 23:15:13 +0530

    Test Service added using autowire

---
+++ src/main/java/com/exam/xpress/service/TestService.java
@@ -0,0 +1,14 @@
+package com.exam.xpress.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by shubhamc on 2/1/17.
+ */
+
+@Service
+public class TestService {
+    public String test(){
+        return "{\"name\":\"Sid\", \"agae\":21}";
+    }
+}
